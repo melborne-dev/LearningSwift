96acef7cc951e462a90ae0bf30b5828fdd84dce8
Melborne Aloj <melbornesmacbookpro@Melbornes-MacBook-Pro.local>
2017-04-20 22:44:25 +0530

    till computed property demo, end of lecture 1

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,14 +9,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-
+    
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInMiddleOfTyping = false
+    
     @IBAction func touchDigit(_ sender: UIButton) {
         
-        var digit = sender.currentTitle
-        
+        let digit = sender.currentTitle!
+        if userIsInMiddleOfTyping {
+            
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        }else{
+            display.text = digit
+            userIsInMiddleOfTyping = true
+        }
         
     }
-
+    
+    var displayValue : Double{
+        get{
+            return Double(display.text!)!
+        }
+        set{
+            display.text = String(newValue)
+        }
+        
+    }
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        userIsInMiddleOfTyping = false
+        if let mathematicalSymbol = sender.currentTitle {
+            switch mathematicalSymbol {
+            case "π":
+                displayValue = (Double.pi)
+            
+            case "√":
+                let operand = Double(display.text!)!
+                displayValue = sqrt(operand)
+            default:
+                break
+            }
+            
+            
+        }
+        
+    }
+    
 }
 
